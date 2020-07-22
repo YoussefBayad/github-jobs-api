bd744ad4fe3b8537ecb9538fc3f0f24f64f69c59
Joseph bayad <jobayad66@gmail.com>
2020-07-22 14:07:36 +0100

    add search forms

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import { Container } from 'react-bootstrap';
 import Job from './components/Job';
 import PagePagination from './components/Pagination';
 import Spinner from './components/Spinner';
+import MySearchForm from './components/SearchForm';
+
 function App() {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(1);
   const { jobs, loading, error, hasNextPage } = FetchJobs(params, page);
+
+  function handleParamChange(e) {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  }
+
   return (
     <Container className="my-6">
       <h1 className="my-4">Github Jobs</h1>
+      <MySearchForm params={params} onParamChange={handleParamChange} />
       <PagePagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <Spinner />}
       {error && <h1>Failed reload </h1>}
